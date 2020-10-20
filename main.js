7c8aa6f60ba9c041cf9fd06127d6f16d99bb57dd
Gregory.Johnson <3531op@gmail.com>
2020-10-20 14:22:19 -0400

    Initial Commit

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 // Your code below
+const isItColin = function (name) {
+  return name === 'Colin' ? 'Yes, that is Colin.' : 'No, that is not Colin.'
+}
 
+const isHot = function (temp) {
+  return temp >= 80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.'
+}
 
+const helloThere = function (name) {
+  return name.length < 6 ? 'Hello, ' + name : 'Hi, ' + name
+}
 
+const goodbyeYou = function (name) {
+  return name ? 'Goodbye, Colin' : 'Goodbye, stranger'
+}
+
+const darkAndStormyNight = function (str, rainFall) {
+  return str === 'dark' && rainFall > 0.5 ? 'It was a dark and stormy night.' : `My usual opening doesn't apply, I guess.`
+}
 
 
 // Our code below; do not touch!
